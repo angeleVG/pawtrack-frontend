fc6c0ee75be9419ddb9bb709cedadb581c3063f2
angeleVG <a.vangroesen@outlook.com>
2025-06-28 13:45:30 +0200

    fix routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 import WeightPage from "./pages/WeightPage";
 import FoodPage from "./pages/FoodPage";
@@ -13,7 +14,6 @@ import DashboardPage from "./pages/DashboardPage";
 import MedicationPage from "./pages/MedicationPage";
 import VaccinationPage from "./pages/VaccinationPage";
 import TaskPage from "./pages/TaskPage";
-
 
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer";
@@ -25,17 +25,9 @@ function App() {
     <div className="App">
       <Navbar />
 
+{/* public routes */}
       <Routes>
         <Route path="/" element={<HomePage />} />
-
-        <Route
-          path="/profile"
-          element={
-            <IsPrivate>
-              <ProfilePage />
-            </IsPrivate>
-          }
-        />
 
         <Route
           path="/signup"
@@ -54,74 +46,77 @@ function App() {
           }
         />
 
- {/* private pagina's (only showing for logged in users) */}
-  <Route
-    path="/profile"
-    element={
-      <IsPrivate>
-        <ProfilePage />
-      </IsPrivate>
-    }
-  />
-  <Route
-    path="/dashboard"
-    element={
-      <IsPrivate>
-        <DashboardPage />
-      </IsPrivate>
-    }
-  />
-  <Route
-    path="/weight"
-    element={
-      <IsPrivate>
-        <WeightPage />
-      </IsPrivate>
-    }
-  />
-  <Route
-    path="/food"
-    element={
-      <IsPrivate>
-        <FoodPage />
-      </IsPrivate>
-    }
-  />
-  <Route
-    path="/contacts"
-    element={
-      <IsPrivate>
-        <ContactPage />
-      </IsPrivate>
-    }
-  />
-  <Route
-    path="/medication"
-    element={
-      <IsPrivate>
-        <MedicationPage />
-      </IsPrivate>
-    }
-  />
-  <Route
-    path="/vaccination"
-    element={
-      <IsPrivate>
-        <VaccinationPage />
-      </IsPrivate>
-    }
-  />
-  <Route
-    path="/task"
-    element={
-      <IsPrivate>
-        <TaskPage />
-      </IsPrivate>
-    }
-  />
+{/* private routes */}
+        <Route
+          path="/profile"
+          element={
+            <IsPrivate>
+              <ProfilePage />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <IsPrivate>
+              <DashboardPage />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/weight"
+          element={
+            <IsPrivate>
+              <WeightPage />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/food"
+          element={
+            <IsPrivate>
+              <FoodPage />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <IsPrivate>
+              <ContactPage />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/medication"
+          element={
+            <IsPrivate>
+              <MedicationPage />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/vaccination"
+          element={
+            <IsPrivate>
+              <VaccinationPage />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/task"
+          element={
+            <IsPrivate>
+              <TaskPage />
+            </IsPrivate>
+          }
+        />
 
+        <Route path="*" element={<NotFoundPage />} />
 
       </Routes>
+      
+      <Footer />
     </div>
   );
 }
