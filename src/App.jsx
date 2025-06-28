66647fe10800977c8210b747436641514e209be0
angeleVG <a.vangroesen@outlook.com>
2025-06-28 13:36:45 +0200

    import new pages and routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
-import HomePage from "./pages/HomePage/HomePage";
-import ProfilePage from "./pages/ProfilePage/ProfilePage";
-import SignupPage from "./pages/SignupPage/SignupPage";
-import LoginPage from "./pages/LoginPage/LoginPage";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+
+import WeightPage from "./pages/WeightPage";
+import FoodPage from "./pages/FoodPage";
+import ContactPage from "./pages/ContactPage";
+import DashboardPage from "./pages/DashboardPage";
+import MedicationPage from "./pages/MedicationPage";
+import VaccinationPage from "./pages/VaccinationPage";
+import TaskPage from "./pages/TaskPage";
+
 
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer";
 import IsPrivate from "./components/IsPrivate/IsPrivate";
 import IsAnon from "./components/IsAnon/IsAnon";
 
@@ -43,6 +53,74 @@ function App() {
             </IsAnon>
           }
         />
+
+ {/* private pagina's (only showing for logged in users) */}
+  <Route
+    path="/profile"
+    element={
+      <IsPrivate>
+        <ProfilePage />
+      </IsPrivate>
+    }
+  />
+  <Route
+    path="/dashboard"
+    element={
+      <IsPrivate>
+        <DashboardPage />
+      </IsPrivate>
+    }
+  />
+  <Route
+    path="/weight"
+    element={
+      <IsPrivate>
+        <WeightPage />
+      </IsPrivate>
+    }
+  />
+  <Route
+    path="/food"
+    element={
+      <IsPrivate>
+        <FoodPage />
+      </IsPrivate>
+    }
+  />
+  <Route
+    path="/contacts"
+    element={
+      <IsPrivate>
+        <ContactPage />
+      </IsPrivate>
+    }
+  />
+  <Route
+    path="/medication"
+    element={
+      <IsPrivate>
+        <MedicationPage />
+      </IsPrivate>
+    }
+  />
+  <Route
+    path="/vaccination"
+    element={
+      <IsPrivate>
+        <VaccinationPage />
+      </IsPrivate>
+    }
+  />
+  <Route
+    path="/task"
+    element={
+      <IsPrivate>
+        <TaskPage />
+      </IsPrivate>
+    }
+  />
+
+
       </Routes>
     </div>
   );
