596c7363c973786538fb714da0bcea96ccbd3279
angeleVG <a.vangroesen@outlook.com>
2025-07-07 20:14:56 +0200

    remove stray newline

--- src/components/PetForm.jsx
+++ src/components/PetForm.jsx
@@ -19,8 +19,7 @@ const navigate = useNavigate();
 
   useEffect(() => {
     axios
-.get(`${process.env.REACT_APP_API_URL}/api/pet/breeds`)
-    .then((res) => {
+.get(`${process.env.REACT_APP_API_URL}/api/pet/breeds`).then((res) => {
       console.log("Received breeds:", res.data); // temporary check
       if (Array.isArray(res.data)) {
         setBreeds(res.data);
