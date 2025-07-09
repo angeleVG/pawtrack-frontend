a1b83a86b9909ee7b3a64bbfd7477b518ab417e7
angeleVG <a.vangroesen@outlook.com>
2025-07-09 20:39:13 +0200

    redirect logged in users to dashboard

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { AuthContext } from "../context/auth.context";
 import { Box, Typography, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 
 const HomePage = () => {
+  const { isLoggedIn } = useContext(AuthContext);
   const navigate = useNavigate();
+
+ useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/dashboard");
+    }
+  }, [isLoggedIn, navigate]);
 
     return (
     <Box
