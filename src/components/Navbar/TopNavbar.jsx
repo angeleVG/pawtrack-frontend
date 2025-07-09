08e1a68224a009b4293c85a86574c8af02e2a2fe
angeleVG <a.vangroesen@outlook.com>
2025-07-09 18:57:51 +0200

    style topnavbar

--- src/components/Navbar/TopNavbar.jsx
+++ src/components/Navbar/TopNavbar.jsx
@@ -1,44 +1,143 @@
+// src/components/layout/TopNavbar.jsx
+import { AppBar, Toolbar, Typography, IconButton, Box, Button, Avatar, Menu, MenuItem } from "@mui/material";
+import PetsIcon from "@mui/icons-material/Pets";
+import LoginIcon from "@mui/icons-material/Login";
+import LogoutIcon from "@mui/icons-material/Logout";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { AuthContext } from "../../context/auth.context";
 
+const COLORS = {
+  background: "#f7fdfc",
+  teal: "#00bfa6",
+  salmon: "#FA8072",
+};
+
 function TopNavbar() {
-  // Subscribe to the AuthContext to gain access to
-  // the values from AuthContext.Provider's `value` prop
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuOpen = (e) => setAnchorEl(e.currentTarget);
+  const handleMenuClose = () => setAnchorEl(null);
 
   return (
-    <nav>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
+    <AppBar
+      position="sticky"
+      elevation={0}
+      sx={{
+        background: COLORS.background,
+        color: COLORS.teal,
+        borderBottom: `2px solid ${COLORS.teal}`,
+        px: 1,
+      }}
+    >
+      <Toolbar sx={{ minHeight: 56, justifyContent: "space-between", p: 0 }}>
+        {/* LEFT: Logo/Titel */}
+        <Box display="flex" alignItems="center">
+          <PetsIcon sx={{ color: COLORS.teal, fontSize: 34, mr: 1 }} />
+          <Typography
+            variant="h6"
+            component={Link}
+            to="/"
+            sx={{
+              textDecoration: "none",
+              color: COLORS.teal,
+              fontWeight: "bold",
+              letterSpacing: 2,
+              fontSize: { xs: "1.1rem", sm: "1.3rem" },
+            }}
+          >
+            PawTrack
+          </Typography>
+        </Box>
 
-      {isLoggedIn && (
-        <>
-          <button onClick={logOutUser}>Logout</button>
-
-          <Link to="/profile">
-            <button>Profile</button>
-            {/* <img src="https://picsum.photos/id/402/200/300" style={{ width: 50, height: 50, borderRadius: 25}} alt="profile" /> */}
-          </Link>
-
-          <span>{user && user.name}</span>
-        </>
-      )}
-
-      {!isLoggedIn && (
-        <>
-          <Link to="/signup">
-            {" "}
-            <button>Sign Up</button>{" "}
-          </Link>
-          <Link to="/login">
-            {" "}
-            <button>Login</button>{" "}
-          </Link>
-        </>
-      )}
-    </nav>
+        {/* RIGHT: Menu/Profile */}
+        <Box display="flex" alignItems="center">
+          {isLoggedIn ? (
+            <>
+              {/* Desktop: Profile Avatar + Logout */}
+              <IconButton
+                onClick={handleMenuOpen}
+                size="large"
+                sx={{ ml: 1 }}
+                color="inherit"
+                aria-label="account"
+              >
+                {user?.image ? (
+                  <Avatar src={user.image} alt={user.name} />
+                ) : (
+                  <AccountCircleIcon sx={{ fontSize: 32 }} />
+                )}
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleMenuClose}
+                anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              >
+                <MenuItem
+                  component={Link}
+                  to="/profile"
+                  onClick={handleMenuClose}
+                >
+                  <AccountCircleIcon sx={{ mr: 1 }} /> Profiel
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    handleMenuClose();
+                    logOutUser();
+                  }}
+                  sx={{ color: COLORS.salmon }}
+                >
+                  <LogoutIcon sx={{ mr: 1 }} /> Uitloggen
+                </MenuItem>
+              </Menu>
+            </>
+          ) : (
+            <>
+              {/* Login */}
+              <Button
+                component={Link}
+                to="/login"
+                startIcon={<LoginIcon />}
+                variant="contained"
+                sx={{
+                  ml: 2,
+                  bgcolor: COLORS.teal,
+                  ":hover": { bgcolor: COLORS.salmon },
+                  fontWeight: 600,
+                  borderRadius: 2,
+                  boxShadow: "none",
+                  textTransform: "none",
+                }}
+              >
+                Inloggen
+              </Button>
+              <Button
+                component={Link}
+                to="/signup"
+                variant="outlined"
+                sx={{
+                  ml: 1,
+                  borderColor: COLORS.teal,
+                  color: COLORS.teal,
+                  fontWeight: 600,
+                  borderRadius: 2,
+                  textTransform: "none",
+                  ":hover": {
+                    bgcolor: "#e0f7f5",
+                    borderColor: COLORS.teal,
+                  },
+                }}
+              >
+                Aanmelden
+              </Button>
+            </>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
 
